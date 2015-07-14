1e9ff806566e57bd64039cedfc4c2e6ada6efdce
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-07-15 00:35:31 +1000

    pretty printing for array computations

--- middle_end/GADT_transforms/Pretty.hs
+++ middle_end/GADT_transforms/Pretty.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleInstances   #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
 -- Pretty printing
@@ -7,25 +8,85 @@
 module Pretty where
 
 import AST
-import Sugar
+import Array.Sugar
 import Text.PrettyPrint.Leijen
 
-
-prettyExp :: forall env t. Int -> (Doc -> Doc) -> OpenExp env t -> Doc
-prettyExp lvl wrap = pp
+prettyAcc
+    :: forall aenv a.
+       Int
+    -> (Doc -> Doc)
+    -> OpenAcc aenv a
+    -> Doc
+prettyAcc alvl wrap = pp
   where
-    ppE :: OpenExp env s -> Doc
-    ppE = prettyExp lvl parens
+    name .$ docs = wrap $ hang 2 (sep (text name : docs))
 
-    pp :: OpenExp env t -> Doc
+    ppE :: OpenExp env aenv t -> Doc
+    ppE = prettyExp 0 alvl parens
+
+    ppF :: OpenFun env aenv f -> Doc
+    ppF = parens . prettyFun 0 alvl
+
+    ppA :: OpenAcc aenv arrs -> Doc
+    ppA = prettyAcc alvl parens
+
+    pp :: OpenAcc aenv arrs -> Doc
+    pp (Use arr)        = wrap (text (show arr))
+    pp (Avar ix)        = char 'a' <> int (alvl - idxToInt ix - 1)
+    pp (Alet a b)       =
+      let x  = char 'a' <> int alvl
+          a' = prettyAcc alvl     id a
+          b' = prettyAcc (alvl+1) id a
+
+          isLet Alet{} = True
+          isLet _      = False
+      in
+      if not (isLet a) && isLet b
+         then wrap $ vcat [ text "let" <+> x <+> equals <+> a' <+> text "in", b'               ]
+         else wrap $ vcat [ text "let" <+> x <+> equals <+> a',               text "in" <+> b' ]
+
+    pp (Map f a)        = "map"      .$ [ ppF f,  ppA a ]
+    pp (Generate sh f)  = "generate" .$ [ ppE sh, ppF f ]
+
+
+prettyFun
+    :: forall env aenv f.
+       Int
+    -> Int
+    -> OpenFun env aenv f
+    -> Doc
+prettyFun lvl alvl fun =
+  let (n, bodyDoc) = count n fun
+  in
+  char '\\' <> hsep [text $ 'x' : show idx | idx <- [lvl..n]] <+>
+  text "->" <+> bodyDoc
+  where
+     count :: Int -> OpenFun env' aenv' fun' -> (Int, Doc)
+     count l (Body body) = (lvl-1, prettyExp (l + 1) alvl id body)
+     count l (Lam  fun') = let (n, body) = count l fun' in (1 + n, body)
+
+
+prettyExp
+    :: forall env aenv t.
+       Int
+    -> Int
+    -> (Doc -> Doc)
+    -> OpenExp env aenv t
+    -> Doc
+prettyExp lvl alvl wrap = pp
+  where
+    ppE :: OpenExp env aenv s -> Doc
+    ppE = prettyExp lvl alvl parens
+
+    pp :: OpenExp env aenv t -> Doc
     pp (Var ix)         = char 'x' <> int (lvl - idxToInt ix - 1)
     pp (Const c)        = text (show (toElt c :: t))
     pp (Prj ix tup)     = wrap (char '#' <> int (prodIdxToInt ix) <+> ppE tup)
 
     pp (Let a b) =
       let x  = char 'x' <> int lvl
-          a' = prettyExp lvl     id a
-          b' = prettyExp (lvl+1) id b
+          a' = prettyExp lvl     alvl id a
+          b' = prettyExp (lvl+1) alvl id b
 
           isLet Let{} = True
           isLet _     = False
@@ -35,9 +96,9 @@ prettyExp lvl wrap = pp
          else vcat [ text "let" <+> x <+> equals <+> a',               text "in" <+> b' ]
 
     pp (Prod p) =
-      let collect :: Prod (OpenExp env) p -> [Doc]
+      let collect :: Prod (OpenExp env aenv) p -> [Doc]
           collect EmptyProd      = []
-          collect (PushProd t e) = prettyExp lvl id e : collect t
+          collect (PushProd t e) = prettyExp lvl alvl id e : collect t
       in
       tupled (reverse (collect p))
 
@@ -53,6 +114,12 @@ prettyExp lvl wrap = pp
       hang 3 $ vcat [ text "if"   <+> ppE p, text "then" <+> ppE t, text "else" <+> ppE e ]
 
 
-instance Show (OpenExp env t) where
-  show = show . prettyExp 0 id
+instance Show (OpenAcc aenv a) where
+  show = show . prettyAcc 0 id
+
+instance Show (OpenFun env aenv f) where
+  show = show . prettyFun 0 0
+
+instance Show (OpenExp env aenv t) where
+  show = show . prettyExp 0 0 id
 
