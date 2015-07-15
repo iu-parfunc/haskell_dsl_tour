5bee3b7656f2937b43edba530d6c47f0c2e4f822
Ryan Newton <rrnewton@gmail.com>
2015-07-15 07:37:17 +0200

    Add directive driven cabal file

--- install_all.sh
+++ install_all.sh
@@ -3,9 +3,11 @@
 set -xe
 
 PKGS=" ./front_end/overloading/ \
+       ./front_end/directive_driven/ \
        ./middle_end/GADT_transforms/Mini-Accelerate.cabal \
        ./middle_end/nanopass/course_example/nanopass-sample.cabal \
        ./middle_end/nanopass/exercise/nanopass-exercise.cabal \
-       ./middle_end/syntactic/nanofeldspar.cabal "
+       ./middle_end/syntactic/nanofeldspar.cabal \
+     "
 
 cabal install -j $PKGS $@
