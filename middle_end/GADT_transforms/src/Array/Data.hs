4a0dea72591d90ec109ded70bf812a6ae2a39643
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-07-15 14:04:42 +1000

    wibbles

--- middle_end/GADT_transforms/src/Array/Data.hs
+++ middle_end/GADT_transforms/src/Array/Data.hs
@@ -1,11 +1,14 @@
 
 -- | Storage for array data
-
+--
 module Array.Data where
 
 import Data.Vector.Unboxed              as U
 
--- mini-accelerate Arrays are just unboxed vectors.
+-- Mini-Accelerate Arrays are just unboxed vectors. We only need vectors of
+-- primitive types (Int, Float, etc.) and do the unzipping of compound
+-- types ourselves.
+--
 type ArrayData e = U.Vector e
 
 class U.Unbox e => ArrayElt e
@@ -14,5 +17,5 @@ instance ArrayElt ()
 instance ArrayElt Int
 instance ArrayElt Float
 instance ArrayElt Bool
-
 instance (ArrayElt a, ArrayElt b) => ArrayElt (a, b)
+
--- middle_end/GADT_transforms/src/Fusion.hs
+++ middle_end/GADT_transforms/src/Fusion.hs
@@ -15,7 +15,7 @@ fuseAcc acc =
   case acc of
     Use xs              -> Use xs
     Avar ix             -> Avar ix
-    Alet bnd body       -> error "TODO: fuseAcc/Alet"
-    Map f xs            -> error "TODO: fuseAcc/Map"
-    Generate sh f       -> error "TODO: fuseAcc/Generate"
+    Alet bnd body       -> error "TODO: Fusion.fuseAcc/Alet"
+    Map f xs            -> error "TODO: Fusion.fuseAcc/Map"
+    Generate sh f       -> error "TODO: Fusion.fuseAcc/Generate"
 
